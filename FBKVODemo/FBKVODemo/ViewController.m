10f151d0aa707a247479627badf518cf355d4ac1
taoge868 <377121930@qq.com>
2017-08-01 17:47:00 +0800

    添加项目

---
+++ FBKVODemo/FBKVODemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FBKVODemo
+//
+//  Created by P2P on 2017/8/1.
+//  Copyright © 2017年 summer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ FBKVODemo/FBKVODemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FBKVODemo
+//
+//  Created by P2P on 2017/8/1.
+//  Copyright © 2017年 summer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
